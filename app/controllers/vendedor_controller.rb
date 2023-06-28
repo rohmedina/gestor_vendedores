8c86e147d625366d428c40b7d0ea3de33a54627c
Adolfo Alvarez <adolfo.alvar3z.p@gmail.com>
2023-06-27 21:56:28 -0400

    update
    Co-authored-by: Ricardo Serra <Ricardo.s14@gmail.com>
    Co-authored-by: RoMedina <rmedinagatica@gmail.com>
    Co-authored-by: yeseniau <yeseniau@users.noreply.github.com>

--- app/controllers/vendedor_controller.rb
+++ app/controllers/vendedor_controller.rb
@@ -1,10 +1,13 @@
 class VendedorController < ApplicationController
   def index
+      @vendedores = Vendedor.all
   end
 
   def new
+    @vendedores = Vendedor.new
   end
 
   def create
+    @vendedores = Vendedor.create(vendedores_params)
   end
 end
